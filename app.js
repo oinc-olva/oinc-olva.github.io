85e5d6ea7140a893e36a40d35b1f1071633f1fe6
oinc-olva <90981882+oinc-olva@users.noreply.github.com>
2022-05-25 13:53:39 +0200

    Aftelling toegevoegd

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ $bgImg.addEventListener('animationend', () => {
 $bgVideo.addEventListener('canplay', () => {
     animationPartsDone++;
     if (animationPartsDone == 2) startVideo();
-})
+});
 function startVideo() {
     $bgImg.classList.add('hide');
     $bgVideo.playbackRate = .6;
@@ -18,4 +18,37 @@ function startVideo() {
 $bgVideo.addEventListener('ended', () => {
     $bgVideo.currentTime = 0;
     $bgVideo.play();
-})
+});
+
+
+
+const $countDown = document.getElementById('countDown');
+const $cdHrs = document.getElementById('cdHrs').getElementsByClassName('cdSegment')[0];
+const $cdMin = document.getElementById('cdMin').getElementsByClassName('cdSegment')[0];
+const $cdSec = document.getElementById('cdSec').getElementsByClassName('cdSegment')[0];
+
+let cdInterval = setInterval(() => {
+    updateCountdown();
+}, 1000)
+
+function updateCountdown() {
+    const d = new Date();
+    let hrs = d.getHours() + 1;
+    let min = d.getMinutes() + 1;
+    let sec = d.getSeconds() + 1;
+    
+    sec = 60 - sec;
+    min = 60 - min;
+    hrs = 16 - hrs;
+
+    if (hrs < 0 || min < 0 || sec < 0) {
+        clearInterval(cdInterval);
+        $countDown.classList.add('done');
+        $countDown.innerText = "De website wordt over enkele ogenblikken gelanceerd...";
+        setTimeout(() => window.location.reload(true), 30000);
+    } else {
+        $cdSec.innerText = sec;
+        $cdMin.innerText = min;
+        $cdHrs.innerText = hrs;
+    }
+}
