46008c8be29aee426c7ac17f08767c759c5641d4
oinc-olva <90981882+oinc-olva@users.noreply.github.com>
2022-08-26 00:54:34 +0200

    Eindschermen gemaakt

--- src/components/video/EndScreen.vue
+++ src/components/video/EndScreen.vue
@@ -1,15 +1,192 @@
 <template>
     <div id="endScreen">
+        <div id="autoPlayScreen" v-if="isAutoplay && !isAutoplayCancelled">
+            <button id="apCloseButton" class="icon" @click="cancelAutoplay" title="Autoplay annuleren" aria-label="Autoplay annuleren" tabindex="4"><fa icon="times" /></button>
+            <div id="apTimerBar">
+                <div id="apTimerProgress"></div>
+            </div>
+            <div id="apMainContent">
+                <h3 id="apTimerSub">Over {{autoplayCountdown}} seconden...</h3>
+                <VideoPreview :video="videos.values[recommendedVideoIds[0]]" :isPlaying="false" tabindex="4" />
+                <div id="apOptions">
+                    <button id="apCancel" class="btn" @click="cancelAutoplay" aria-label="Autoplay annuleren" tabindex="4">Annuleren</button>
+                    <button id="apContinue" class="btn" @click="continueAutoplay" aria-label="Autoplay verderzetten" tabindex="4">Afspelen</button>
+                </div>
+            </div>
+        </div>
+        <div id="recommendedScreen">
+            <h3 id="rsTitle">Misschien kijkt u ook naar...</h3>
+            <ul id="rsList" aria-label="Suggesties" tabindex="4">
+                <li v-for="videoId of ( !isAutoplay || isAutoplayCancelled ? recommendedVideoIds: recommendedVideoIds.slice(1) )" :key="videoId">
+                    <VideoPreview :video="videos.values[videoId]" :isPlaying="false" tabindex="4" />
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
+import VideoPreview from '../videos/VideoPreview.vue'
+
 export default {
     name: 'EndScreen',
+    components: {
+        VideoPreview
+    },
     props: {
         videos: Object,
         recommendedVideoIds: Array,
         isAutoplay: Boolean
+    },
+    emits: [ 'setCurrentVideoId' ],
+    data() {
+        return {
+            isAutoplayCancelled: false,
+            autoplayInterval: null,
+            autoplayCountdown: 10
+        }
+    },
+    methods: {
+        startAutoplay() {
+            this.autoplayInterval = setInterval(() => {
+                this.autoplayCountdown--;
+                if (this.autoplayCountdown == 0) {
+                    clearInterval(this.autoplayInterval);
+                    this.continueAutoplay();
+                }
+            }, 1000);
+        },
+        cancelAutoplay() {
+            this.isAutoplayCancelled = true;
+            clearInterval(this.autoplayInterval);
+        },
+        continueAutoplay() {
+            if (this.$route.name == 'Video') {
+                this.$router.push({
+                    name: 'Video',
+                    path: '/videos/:videoId/:videoPath',
+                    params: {
+                        videoId: this.recommendedVideoIds[0],
+                        videoPath: this.videos.values[this.recommendedVideoIds[0]].videoPath
+                    }
+                });
+            } else {
+                this.$emit('setCurrentVideoId', this.recommendedVideoIds[0]);
+            }
+        },
+        escapePressed(e) {
+            if (e.key == 'Escape' && !this.isAutoplayCancelled) this.cancelAutoplay();
+        }
+    },
+    mounted() {
+        if (this.isAutoplay) this.startAutoplay();
+        document.addEventListener('keyup', this.escapePressed, false);
+    },
+    unmounted() {
+        clearInterval(this.autoplayInterval)
+        document.removeEventListener('keyup', this.escapePressed, false);
+    },
+    watch: {
+        isAutoplay(bool) {
+            if (!bool) {
+                clearInterval(this.autoplayInterval);
+            } else {
+                if (!this.isAutoplayCancelled) {
+                    this.autoplayCountdown = 10;
+                    this.startAutoplay();
+                }
+            }
+        }
     }
 }
 </script>
+
+<style lang="scss" scoped>
+    $screenPadding: 40px;
+    $apCloseButtonSize: 1.2em;
+
+    #endScreen {
+        display: flex;
+        background: $videoBackground;
+        overflow: hidden;
+        color: $headingColor;
+    }
+    #autoPlayScreen {
+        position: relative;
+        flex: 1;
+        background: $videoBackgroundBrighter;
+        padding: $screenPadding;
+
+
+        h3 { font-size: 1em; }
+        #apCloseButton {
+            position: absolute;
+            top: $screenPadding;
+            right: $screenPadding;
+            font-size: $apCloseButtonSize;
+        }
+        #apTimerBar {
+            width: calc(100% - #{$apCloseButtonSize + .7em});
+            height: 4px;
+            background: rgba(255, 255, 255, .1);
+            margin-top: #{$apCloseButtonSize * .5};
+            margin-bottom: 15px;
+
+            #apTimerProgress {
+                height: 100%;
+                background-color: $accentColor;
+                animation: countDown 10s linear;
+                @keyframes countDown {
+                    from { width: 0; }
+                    to { width: 100%; }
+                }
+            }
+        }
+        #apMainContent {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            height: calc(100% - #{60px + $screenPadding * .5});
+        }
+        .videoPreview {
+            width: 100%;
+            padding: 10%;
+        }
+        #apOptions {
+            display: flex;
+            width: 100%;
+
+            button {
+                margin: 0 20px;
+                flex: 1;
+            }
+            #apCancel {
+                background: #333749;
+            }
+        }
+    }
+    #recommendedScreen {
+        display: flex;
+        flex-direction: column;
+        flex: 2;
+        background: $videoBackground;
+        padding: $screenPadding;
+
+        #rsList {
+            display: grid;
+            grid-template-columns: repeat(auto-fill, minmax(calc(200px + 7%), 1fr));
+            padding: 0 calc(5px + 3%);
+            list-style: none;
+            height: 0;
+            flex: 1;
+            margin: 40px 0;
+            overflow-y: scroll;
+
+            li a {
+                width: 100%;
+                padding: 20px 40px;
+                outline-offset: -10px;
+            }
+        }
+    }
+</style>
--- src/components/video/VideoPlayer.vue
+++ src/components/video/VideoPlayer.vue
@@ -10,7 +10,7 @@
                             <button class="close icon" tabindex="3" aria-label="Afsluiten" @click.stop="close"><fa icon="times" /></button>
                             <button class="expand icon" tabindex="2" aria-label="Vergroten" @click.stop="expand"><fa icon="external-link-alt" rotation="270" /></button>
                             <div class="controls">
-                                <button class="pausePlay icon" tabindex="5" :aria-label="isPaused ? 'Afspelen' : 'Pauzeren'" @click.stop="pausePlay(false)"><fa :icon="isPaused ? 'play' : 'pause'" /></button>
+                                <button class="pausePlay icon" tabindex="6" :aria-label="isPaused ? 'Afspelen' : 'Pauzeren'" @click.stop="pausePlay(false)"><fa :icon="isPaused ? 'play' : 'pause'" /></button>
                                 <button class="volume icon" tabindex="-1" aria-label="Volume" @mouseover="this.isVolumeWrapperOpen = true"><fa :icon="this.isMuted ? 'volume-mute' : (this.volume == 0 ? 'volume-off' : (this.volume < 70 ? 'volume-down' : 'volume-up'))" /></button>
                                 <div id="time" v-if="$refs.youtube">
                                     <span id="currentTime">{{videoTimeSecFormatted}}</span>
@@ -18,10 +18,10 @@
                                     <span id="maxTime">{{video.durationFormatted}}</span>
                                 </div>
                                 <div class="floatRight" v-if="isOnVideoPage">
-                                    <button class="youtubeBtn icon" tabindex="8" aria-label="Op YouTube bekijken" @click.stop="watchOnYoutube"><fa :icon="['fab', 'youtube']" /></button>
-                                    <button class="playbackRate icon" tabindex="8" ref="playbackRateBtn" aria-label="Snelheid" @click.stop="togglePlaybackRateModal"><fa icon="tachometer-alt" /></button>
-                                    <button class="miniplayer icon" tabindex="10" aria-label="Minimalizeren" @click="gotoVideos"><fa icon="external-link-alt" rotation="90" /></button>
-                                    <button class="fullscreen icon" tabindex="10" aria-label="Volledig scherm" @click="toggleFullscreenMode"><fa :icon="this.isInFullscreenMode ? 'compress' : 'expand'" /></button>
+                                    <button class="youtubeBtn icon" tabindex="9" aria-label="Op YouTube bekijken" @click.stop="watchOnYoutube"><fa :icon="['fab', 'youtube']" /></button>
+                                    <button class="playbackRate icon" tabindex="9" ref="playbackRateBtn" aria-label="Snelheid" @click.stop="togglePlaybackRateModal"><fa icon="tachometer-alt" /></button>
+                                    <button class="miniplayer icon" tabindex="11" aria-label="Minimalizeren" @click="gotoVideos"><fa icon="external-link-alt" rotation="90" /></button>
+                                    <button class="fullscreen icon" tabindex="11" aria-label="Volledig scherm" @click="toggleFullscreenMode"><fa :icon="this.isInFullscreenMode ? 'compress' : 'expand'" /></button>
                                 </div>
                             </div>
                         </div>
@@ -32,26 +32,26 @@
                         </div>
                     </div>
                     <div id="volumeSliderOuterWrapper" :class="{open: isVolumeWrapperOpen}" ref="volumeSliderOuterWrapper" @mouseleave="mouseLeaveVolumeSliderWrapper">
-                        <div id="volumeSliderInnerWrapper" tabindex="7" @mousedown.left="startDragVolumeSlider" @keydown="sliderVolumeKeydown" role="slider" aria-label="volume" aria-valuemin="0" aria-valuemax="100" :aria-valuenow="volume" :aria-valuetext="`${this.volume}%`">
+                        <div id="volumeSliderInnerWrapper" tabindex="8" @mousedown.left="startDragVolumeSlider" @keydown="sliderVolumeKeydown" role="slider" aria-label="volume" aria-valuemin="0" aria-valuemax="100" :aria-valuenow="volume" :aria-valuetext="`${this.volume}%`">
                             <div id="volumeSlider" ref="volumeSlider">
                                 <div id="volumeSliderLevel" :style="{height: this.isMuted ? '0' : this.volume + '%'}" />
                             </div>
                         </div>
-                        <button class="muteAudio icon" tabindex="6" :aria-label="isMuted ? 'Geluid inschakelen' : 'Geluid uitschakelen'" @click="toggleMute"></button>
+                        <button class="muteAudio icon" tabindex="7" :aria-label="isMuted ? 'Geluid inschakelen' : 'Geluid uitschakelen'" @click="toggleMute"></button>
                     </div>
                     <div id="playbackRateModal" ref="playbackRateModal" v-show="isPlaybackRateModalOpen" @keydown.esc.stop="togglePlaybackRateModal">
                         <ul role="listbox" aria-label="Snelheid kiezen">
-                            <li :key="option" v-for="option in availablePlaybackRates" :class="{selected: this.playbackRate == option}" tabindex="9" @click="setPlaybackRate(option)" @keydown.enter.space.prevent="setPlaybackRate(option)" role="option" :aria-label="option == 1 ? 'originele snelheid' : `${option*100}% van originele snelheid`" :aria-selected="this.playbackRate == option">{{option}}</li>
+                            <li :key="option" v-for="option in availablePlaybackRates" :class="{selected: this.playbackRate == option}" tabindex="10" @click="setPlaybackRate(option)" @keydown.enter.space.prevent="setPlaybackRate(option)" role="option" :aria-label="option == 1 ? 'originele snelheid' : `${option*100}% van originele snelheid`" :aria-selected="this.playbackRate == option">{{option}}</li>
                         </ul>
                     </div>
-                    <div id="timeline" :tabindex="this.isOnVideoPage ? 4 : 11" :class="{dragging: this.draggingType == 1}" ref="timeline" @mousemove="calculateHoveredTimelineTime" @mousedown.left="startDragTimeline" @keydown="sliderTimelineKeydown" role="slider" aria-label="tijdlijn" aria-valuemin="0" :aria-valuemax="video.durationSec" :aria-valuenow="videoTimeSec" :aria-valuetext="timelineAriaText">
+                    <div id="timeline" :tabindex="this.isOnVideoPage ? 5 : 12" :class="{dragging: this.draggingType == 1}" ref="timeline" @mousemove="calculateHoveredTimelineTime" @mousedown.left="startDragTimeline" @keydown="sliderTimelineKeydown" role="slider" aria-label="tijdlijn" aria-valuemin="0" :aria-valuemax="video.durationSec" :aria-valuenow="videoTimeSec" :aria-valuetext="timelineAriaText">
                         <div id="tlBackground"></div>
                         <div id="tlBuffered" :style="{width: this.videoLoadedFrac * 100 + '%'}"></div>
                         <div id="tlProgress" :style="{width: this.videoTimeSec / this.video.durationSec * 100 + '%'}"></div>
                         <div id="tlHoverTimeTooltip" :style="{left: this.relativeMouseX + 'px'}">{{this.videoTimeHoveringFormatted}}</div>
                     </div>
                 </div>
-                <EndScreen v-if="currentPlayerState == 0" :videos="videos" :recommendedVideoIds="recommendedVideoIds" :isAutoplay="isAutoplay" />
+                <EndScreen v-if="currentPlayerState == 0" :videos="videos" :recommendedVideoIds="recommendedVideoIds" :isAutoplay="isAutoplay" @setCurrentVideoId="videoId => $emit('setCurrentVideoId', videoId)" />
                 <div id="error" v-if="errorVal != 0">
                     <div class="overlay">
                         <button class="close icon" aria-label="Afsluiten" @click.stop="close"><fa icon="times" /></button>
@@ -419,6 +419,7 @@ export default {
         },
         toggledFullscreen() {
             this.isInFullscreenMode = document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement;
+            if (!this.isInFullscreenMode) document.activeElement.blur();
         },
         togglePlaybackRateModal() {
             this.isPlaybackRateModalOpen = !this.isPlaybackRateModalOpen;
