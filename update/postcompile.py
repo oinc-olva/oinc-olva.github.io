809af679695079d682cf4d0de25d485ae99e7799
oinc-olva <90981882+oinc-olva@users.noreply.github.com>
2022-08-28 17:58:35 +0200

    De head tags worden nu anders gegenereerd

--- update/postcompile.py
+++ update/postcompile.py
@@ -1,24 +1,18 @@
 #!/usr/bin/env python3
 import os
+import re
 import glob
 
 def main():
     # Kopieer gegenereerde head tags
     with open(os.path.abspath('dist/index.html'), 'r') as f:
-        html_head_tags = f.read()[6:-7]
+        html_head_tags = re.findall(r"\.ico\"\>(.*?)\<\/head\>", f.read())[0]
     
     # Sla op in ander bestand
     f = open(os.path.abspath('dist/htmlHeadTags.html'), "w+")
     f.write(html_head_tags)
     f.close()
     print('Found and saved HTML head tags')
-
-    # Kopieer index.html van /public naar /dist
-    with open(os.path.abspath('public/index.html'), 'r') as src:
-        dest = open(os.path.abspath('dist/index.html'), "w+")
-        dest.write(src.read().replace('$headTags', html_head_tags))
-        dest.close()
-    print('Copied public/index.html --> dist/index.html and (possibly) injected head tags')
     
     # Update (bijna) alle html-bestanden in /dist
     html_files = glob.glob('dist/[!generated]*/**/*.html', recursive=True)
@@ -33,9 +27,6 @@ def main():
         f.write(src.replace('$headTags', html_head_tags))
         f.close()
     print(f"Injected HTML head tags in (at most) {len(html_files)} files")
-
-    # Verwijder public/htmlHeadTags.html
-    os.remove(os.path.abspath('public/htmlHeadTags.html'))
             
 
 if __name__ == "__main__":
--- vue.config.js
+++ vue.config.js
@@ -11,11 +11,6 @@ module.exports = {
         }
     },
     chainWebpack: config => {
-        config.plugin('html')
-            .tap(args => {
-                args[0].template = path.resolve(__dirname, 'public/htmlHeadTags.html')
-                return args
-            });
         config.plugin('copy')
             .tap(args => {
                 args[0].patterns[0].globOptions.ignore.push( path.resolve(__dirname, 'public/htmlHeadTags.html').replace(/\\/g, '/') );
